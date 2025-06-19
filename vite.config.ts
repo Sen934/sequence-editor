ad6b63b404bb2aa203bfd094a181e64c930c6978
Sen934 <46794874+Sen934@users.noreply.github.com>
2025-06-19 15:52:06 +0200

    Add SVGR

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from '@svgr/rollup';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
